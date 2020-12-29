20c67427af14707c95ed80bfb9c04db9273aa295
Eran Peled <eranltd@gmail.com>
2020-12-29 08:39:26 +0200

    first_upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+// import routes
+const queueRoutes = require('./routes/auth');
+
+// app middlewares
+app.use(morgan('dev'));
+// app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '5mb', type: 'application/json' }));
+// app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_URL }));
+
+// middlewares
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', linkRoutes);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`API is running on port ${port}`));
